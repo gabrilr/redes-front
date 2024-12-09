2e7eae4e1c271d1487c2993f754009d2d70784d1
gabrilr <gabrilreyes30@gmail.com>
2024-12-09 11:17:03 -0600

    cambio de rutas

--- src/pages/RegistroView.jsx
+++ src/pages/RegistroView.jsx
@@ -17,7 +17,7 @@ const RegistroView = () => {
       return;
     }
 
-    const response = await fetch('http://3.219.37.136:3000/auth/register', {
+    const response = await fetch('http://127.0.0.1:3000/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
